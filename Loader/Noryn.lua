32c44cc2955692c811df4ed0a501657d26c01d2f
Nory <amiroamiro0981@gmail.com>
2026-05-21 18:46:37 +0400

    Refactor Noryn.lua to load resources dynamically
    Refactor to return a function that loads a resource from a constructed URL.

--- Loader/Noryn.lua
+++ Loader/Noryn.lua
@@ -1,1 +1,12 @@
-print('Hello World!')
+return (function(...)
+    local Noryn, On, Top = 'workers', 'dev', 'Assets'
+
+    local Run = `{Noryn}.{On}/{Top}`
+    local Path = `http://noryn.loader-dll.{Run}`
+
+    local Loader = (loadstring or load)
+    local Http = (syn and syn.request) or (http and http.request) or http_request or request
+    local Http_Response = Http and Http({Url = Path, Method = 'GET'}) or {Body = game:HttpGet(Path)}
+    local Response = Http_Response.Body
+    return Loader(Response)(..., 'dsc.gg/getnoryn — ✨')
+end)(...)
